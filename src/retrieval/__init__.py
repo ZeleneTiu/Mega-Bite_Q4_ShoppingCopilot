606565f83231470fc8fcd494fb0dca5af875f8b7
ZeleneTiu <zelene.tiu.2024@computing.smu.edu.sg>
2026-09-01 01:10:24 +0800

    Remove Nithiesh's stray files from src/retrieval (they live in src/rerank now); restore clean retrieval package

--- src/retrieval/__init__.py
+++
@@ -1,5 +0,0 @@
-from .reranker import Reranker
-from .contract import build_response, validate_response, ContractViolation
-from .llm_client import LLMClient
-
-__all__ = ["Reranker", "build_response", "validate_response", "ContractViolation", "LLMClient"]
